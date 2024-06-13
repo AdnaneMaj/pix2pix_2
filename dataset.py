c6e480a3499d77f535aa7b7bf7ae5ec54b148ce2
MAJDOUB Adnane <52354033+AdnaneMaj@users.noreply.github.com>
2024-06-13 18:09:03 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -48,7 +48,7 @@ class Costum(Dataset):
         self.list_files_rgb = os.listdir(root_dir+"rgb/images/") if not val else os.listdir(root_dir+"rgb_val/images/")
 
     def __len__(self):
-        return len(self.list_files_seg)*100
+        return len(self.list_files_rgb)*100
     
     def __getitem__(self,idx):
         idx = idx%len(self.list_files_seg)
