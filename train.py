f112eb03bdeb01820a4669a122dd74cc472af563
MAJDOUB Adnane <52354033+AdnaneMaj@users.noreply.github.com>
2024-06-13 18:14:15 +0100

    Update train.py

--- train.py
+++ train.py
@@ -77,4 +77,4 @@ def main():
             save_checkpoint(gen,opt_gen,filename=config.CHECKPOINT_GEN)
             save_checkpoint(disc,opt_disc,filename=config.CHECKPOINT_DISC)
 
-        save_some_examples(gen,val_loader,epoch,folder="/kaggle/working/evaluation")
+        save_some_examples(gen,val_loader,epoch,folder="/kaggle/working/")
